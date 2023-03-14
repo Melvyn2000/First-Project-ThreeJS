eb14673686766e67e6957bfb044e45a3f71107f6
Melvyn <garnier.melv@hotmail.com>
2023-03-14 23:40:51 +0100

    Initial commit 🎉

---
+++ src/main.js
@@ -0,0 +1,35 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import * as THREE from 'three'
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg')
+});
+const geometry = new THREE.TorusGeometry(10, 3, 10, 100);
+const material = new THREE.MeshBasicMaterial({
+    color: 0xFF6347,
+    wireframe: true
+});
+const torus = new THREE.Mesh(geometry, material);
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
+scene.add(torus);
+
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+    torus.rotation.x += 0.01;
+    torus.rotation.y += 0.005;
+    torus.rotation.y += 0.01;
+}
+
+animate();
+
+createApp(App).mount('#app')
